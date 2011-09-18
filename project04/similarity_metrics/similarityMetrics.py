08de7284c062cd15bc06a639234fb145fb4fb831
David Danford <ddanford@gmail.com>
2011-09-18 09:40:06 -0600

    Finished Simple Matching Coefficient.

--- project04/similarity_metrics/similarityMetrics.py
+++ project04/similarity_metrics/similarityMetrics.py
@@ -21,7 +21,22 @@ def smc(subject, target):
 	if len(subject) != len(target):
 		print 'Paramters must have the same number of attributes.\n'
 		return
-		
+	num00 = 0
+	num01 = 0
+	num10 = 0
+	num11 = 0
+	for attr in subject:
+		if subject[attr]==0 and target[attr]==0:
+			num00++
+		elif subject[attr]==0 and target[attr]==1:
+			num01++
+		elif subject[attr]==1 and target[attr]==0:
+			num10++
+		elif subject[attr]==1 and target[attr]==1:
+			num11++
+	
+	similarity = (num00+num11)/len(subject)
+	return similarity
 
 def jaccard(subject, target):
 	if type(subject) != type([]) or type(target) != type([]):
