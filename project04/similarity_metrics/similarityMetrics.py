cb9e70e26ef6f693212b0ece7b161a7d354f7a9a
David Danford <ddanford@gmail.com>
2011-09-18 10:03:53 -0600

    Finished Pearson Correlation Coefficient and fixed minor bugs.

--- project04/similarity_metrics/similarityMetrics.py
+++ project04/similarity_metrics/similarityMetrics.py
@@ -8,7 +8,7 @@ def euclidean(subject, target):
 		print 'Paramters must have the same number of attributes.\n'
 		return
 	distance = 0
-	for attr in subject:
+	for attr in range(0:len(subject)):
 		distance += (subject[attr]-target[attr])**2
 	
 	distance = sqrt(distance)
@@ -25,7 +25,7 @@ def smc(subject, target):
 	num01 = 0
 	num10 = 0
 	num11 = 0
-	for attr in subject:
+	for attr in range(0:len(subject)):
 		if subject[attr]==0 and target[attr]==0:
 			num00++
 		elif subject[attr]==0 and target[attr]==1:
@@ -49,7 +49,7 @@ def jaccard(subject, target):
 	num01 = 0
 	num10 = 0
 	num11 = 0
-	for attr in subject:
+	for attr in range(0:len(subject)):
 		if subject[attr]==0 and target[attr]==0:
 			num00++
 		elif subject[attr]==0 and target[attr]==1:
@@ -62,6 +62,15 @@ def jaccard(subject, target):
 	similarity = num11/(num01+num10+num11)
 	return similarity
 
+def stdev(list):
+	mean = sum(list)/len(list)
+	std = 0
+	for l in list:
+		std += (l-mean)**2
+	
+	std = sqrt(std/len(list))
+	return std
+	
 def pearson(subject, target):
 	if type(subject) != type([]) or type(target) != type([]):
 		print 'Parameters must both be lists of attributes.\n'
@@ -69,6 +78,15 @@ def pearson(subject, target):
 	if len(subject) != len(target):
 		print 'Paramters must have the same number of attributes.\n'
 		return
+	sbar = sum(subject)/len(subject)
+	tbar = sum(subject)/len(subject)
+	sstd = stdev(subject)
+	tstd = stdev(target)
+	pcc = 0
+	for attr in range(0:len(subject)):
+		pcc = (subject[attr]-sbar)/sstd*(target[attr]-tbar)/tstd
+	pcc = (len(subject)-1)*pcc
+	return pcc
 
 def cosine(subject, target):
 	if type(subject) != type([]) or type(target) != type([]):
