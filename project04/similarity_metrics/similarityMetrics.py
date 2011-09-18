cb65c42e01e2f619d267074f69e3500aa59bd165
David Danford <ddanford@gmail.com>
2011-09-18 09:42:43 -0600

    Finished Jaccard similarity coefficient.  Yay copy/paste!

--- project04/similarity_metrics/similarityMetrics.py
+++ project04/similarity_metrics/similarityMetrics.py
@@ -45,6 +45,22 @@ def jaccard(subject, target):
 	if len(subject) != len(target):
 		print 'Paramters must have the same number of attributes.\n'
 		return
+	num00 = 0
+	num01 = 0
+	num10 = 0
+	num11 = 0
+	for attr in subject:
+		if subject[attr]==0 and target[attr]==0:
+			num00++
+		elif subject[attr]==0 and target[attr]==1:
+			num01++
+		elif subject[attr]==1 and target[attr]==0:
+			num10++
+		elif subject[attr]==1 and target[attr]==1:
+			num11++
+	
+	similarity = num11/(num01+num10+num11)
+	return similarity
 
 def pearson(subject, target):
 	if type(subject) != type([]) or type(target) != type([]):
