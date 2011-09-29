64b7461b789da56af4760aa38cc6250dfb12bc66
David Danford <ddanford@gmail.com>
2011-09-28 23:01:31 -0600

    Comes up with a reasonable solution most of the time.

--- project05/kmeans.py
+++ project05/kmeans.py
@@ -6,84 +6,120 @@ def read_csv(filename):
 	dataobjects = []
 	filelines = open(filename).readlines()
 	for line in filelines:
-		attributes = line.split(',')
-		for i in range(0, len(attributes)-1):
-			attributes[i] = float(attributes[i])
-		if len(attributes) != 1:
+		splitline = line.split(',')
+		attributes = [float(splitline[i]) for i in range(0,len(splitline)-1)]
+		if len(attributes) != 0:
 			dataobjects.append(attributes)
 	return dataobjects
 	
-def randomCentroid(numAttr, mins, maxs):
-	random.seed()
-	centroid = []
-	for i in range(0,numAttr):
-		centroid.append( random.random()*(maxs[i]-mins[i])+mins[i] )
-	return centroid
+def randomCentroids(k, numAttr, mins, maxs):
+	centroids = []
+	for i in range(0,k):
+		centroid = []
+		random.seed()
+		for j in range(0,numAttr):
+			centroid.append(random.random()*(maxs[j]-mins[j])+mins[j])
+		centroids.append(centroid)
+	print centroids[0]
+	print centroids[1]
+	print centroids[2]
+	return centroids
 
 def findMins(dataobjects, numAttr):
-	minAttrs = dataobjects[0][0:numAttr]
+	minAttrs = dataobjects[0]
 	for i in range(1,len(dataobjects)):
 		for j in range(0,numAttr):
 			if dataobjects[i][j] < minAttrs[j]:
 				minAttrs[j] = dataobjects[i][j]
+	print minAttrs,'Raage'
 	return minAttrs
 	
 def findMaxs(dataobjects, numAttr):
-	maxAttrs = dataobjects[0][0:numAttr]
+	maxAttrs = dataobjects[0]
 	for i in range(1,len(dataobjects)):
 		for j in range(0,numAttr):
 			if dataobjects[i][j] > maxAttrs[j]:
 				maxAttrs[j] = dataobjects[i][j]
+	print maxAttrs,'Raage'
 	return maxAttrs
-
-def kmeans(k, dataobjects, similarityMetric):
+	
+def emptyClusters(k):
 	clusters = []
-	centroids = []
-	numAttr = len(dataobjects[0])-1
-	minAttrs = findMins(dataobjects, numAttr)
-	maxAttrs = findMaxs(dataobjects, numAttr)
-	
-	
-	
 	for i in range(0,k):
 		clusters.append([])
-		centroids.append(randomCentroid(numAttr, minAttrs, maxAttrs))
+	return clusters
 	
-	clusters[0] = dataobjects
-	changed = True
-	maxIterations = 1000
-	iterations=0
-	while(changed and iterations<maxIterations):
-		changed = False
-		for obj in dataobjects:
-			s = 0
-			for i in range(0,k):
-				sim = similarityMetric(obj[0:numAttr],centroids[i])
-				if sim == 0:
-					print 'huh?'
-				if sim > s:
-					s = sim
-					if not (obj in clusters[i]):
-						changed = True
-						for j in range(0,k):
-							if obj in clusters[j]:
-								clusters[j].remove(obj)
-						clusters[i].append(obj)
+def assignObjects(dataobjects, similarityMetric, centroids):
+	k = len(centroids)
+	clusters = emptyClusters(k)
+	numAttrs = len(dataobjects[0])
+	
+	for obj in dataobjects:
+		maxSim = 0
+		clusterIndex = -1
 		for i in range(0,k):
-			for j in range(0,numAttr):
-				if(len(clusters[i])!=0):
-					centroids[i][j]=0
+			sim = similarityMetric(obj, centroids[i])
+			#print i,' ',maxSim,' ',sim
+			if sim > maxSim:
+				maxSim = sim
+				clusterIndex = i
+		clusters[clusterIndex].append(obj)
+	return clusters
+	
+def zeroCentroid(attrs):
+	c = []
+	for i in range(0,attrs):
+		c.append(0)
+	return c
+	
+def moveCentroids(clusters, numAttrs):
+	centroids = emptyClusters(len(clusters))
+	for c in centroids:
+		c = zeroCentroid(numAttrs)
+	for i in range(0,len(clusters)):
+		clusterSize = len(clusters[i])
+		attrSum = zeroCentroid(numAttrs)
+		for obj in clusters[i]:
+			for j in range(0,numAttrs):
+				attrSum[j] += obj[j]
+		centroids[i] = attrSum
+		if clusterSize > 0:
+			centroids[i] = [a/clusterSize for a in attrSum]
+	return centroids
+
+def randomCentroidPoints(k, dataobjects, numAttrs):
+	centroids = []
+	for i in range(0,k):
+		centroids.append(dataobjects[int(math.ceil(random.random()*len(dataobjects)))])
+	return centroids
+	
+def kmeans(k, dataobjects, similarityMetric):
+	numAttr = len(dataobjects[0])
+	# minAttrs = findMins(dataobjects, numAttr)
+	# print minAttrs
+	# maxAttrs = findMaxs(dataobjects, numAttr)
+	# print minAttrs,'Double raage'
+	
+	centroids = randomCentroidPoints(k, dataobjects, numAttr)
+	print centroids
+	clusters = assignObjects(dataobjects, similarityMetric, centroids)
+	
+	changing = True
+	maxIt = 1000
+	it=0
+	while(changing and it<maxIt):
+		changing = False
+		centroids = moveCentroids(clusters, numAttr)
+		newclusters = assignObjects(dataobjects, similarityMetric, centroids) 
+		
+		for i in range(0,k):
 			for obj in clusters[i]:
-				for j in range(0,numAttr):
-					centroids[i][j] += obj[j]
-			for j in range(0,numAttr):
-				if(len(clusters[i])!=0):
-					centroids[i][j] = centroids[i][j]/len(clusters[i])
-		iterations += 1
-		print len(clusters[0])
-		print len(clusters[1])
-		print len(clusters[2])
-	print iterations
+				if not obj in newclusters[i]:
+					changing = True
+		
+		clusters = newclusters
+		it += 1
+	
 	return clusters
 	
 def sse(cluster):
